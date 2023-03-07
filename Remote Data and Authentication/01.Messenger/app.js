20fbb1985f1517f1d2703e678439000566e5e650
Martin Dogandzhiyski <94994594+MartinDogandzhiyski@users.noreply.github.com>
2023-03-07 19:36:03 +0200

    up

--- Remote Data and Authentication/01.Messenger/app.js
+++ Remote Data and Authentication/01.Messenger/app.js
@@ -1,5 +1,27 @@
 function attachEvents() {
-    console.log('TODO...');
+    document.getElementById('submit').addEventListener('click', addComment);
+    document.getElementById('refresh').addEventListener('click', displayAllComments);
+
+}
+
+const messengerUrl = 'http://localhost:3030/jsonstore/messenger';
+
+function addComment() {
+
+}
+
+function displayAllComments() {
+    fetch(messengerUrl).then(res => {
+        if (!res.ok) {
+            throw new Error('Error')
+        }
+        return res.json();
+    }).then( data =>{
+        const textArea = document.querySelector('#messages')
+        const allComments = [];
+        Object.values(data).forEach(c => allComments.push(`${c.author}: ${c.content}`))
+        textArea.value = allComments.join('\n')
+    })
 }
 
 attachEvents();
