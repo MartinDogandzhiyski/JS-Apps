9e83af94b336f9f2833344c3b691d631f6f18682
Martin Dogandzhiyski <94994594+MartinDogandzhiyski@users.noreply.github.com>
2023-03-31 12:46:16 +0300

    up

--- exam_prep_js_apps_3/src/views/details.js
+++ exam_prep_js_apps_3/src/views/details.js
@@ -1,59 +1,54 @@
 import { html } from "../../node_modules/lit-html/lit-html.js";
-import { deleteProduct, getProductById } from "../data/services.js"
+import { deleteProduct, getProductById } from "../data/services.js";
 import { getUserData } from "../util.js";
-
-
-const detailsTemplate = (product, isOwner, getProductById, onDelete) => html`
+ 
+const detailsTemplate = (product, onDelete) => html`
 <section id="details">
-          <div id="details-wrapper">
-            <img id="details-img" src="${product.imageUrl}" alt="example1" />
-            <p id="details-title">${product.title}</p>
-            <p id="details-category">
-              Category: <span id="categories">${product.category}</span>
-            </p>
-            <p id="details-price">
-              Price: <span id="price-number">${product.price}</span>$</p>
-            <div id="info-wrapper">
-              <div id="details-description">
-                <h4>Bought: <span id="buys">0</span> times.</h4>
-                <span
-                  >${product.description}</span
-                >
-              </div>
-            </div>
-            ${productControlTemplate(product, isOwner, onDelete)}
-
-
-              <!--Bonus - Only for logged-in users ( not authors )-->
-              <a href="" id="buy-btn">Buy</a>
-            </div>
-          </div>
-        </section>
-`
-
-const productControlTemplate = (product, isOwner, onDelete) => {
-    if (isOwner) {
-        return html`
-        <div id="action-buttons">
-              <a href="/edit/${product._id}" id="edit-btn">Edit</a>
-              <a @click=${onDelete} href="javascript:void(0)" id="delete-btn">Delete</a>
-        `
-    } else {
-        return null;
-    }
-}
-
-
+<div id="details-wrapper">
+  <img id="details-img" src="${product.imageUrl}" alt="example1" />
+  <p id="details-title">${product.title}</p>
+  <p id="details-category">
+    Category: <span id="categories">${product.category}</span>
+  </p>
+  <p id="details-price">
+    Price: <span id="price-number">${product.price}</span>$</p>
+  <div id="info-wrapper">
+    <div id="details-description">
+      <h4>Bought: <span id="buys">0</span> times.</h4>
+      <span>${product.description}</span>
+    </div>
+  </div>
+  <div id="action-buttons">   
+    ${product.canEdit ? html` 
+    <a href="/details/${product._id}/edit" id="edit-btn">Edit</a>
+    <a @click=${onDelete} href="javascript:void(0)" id="delete-btn">Delete</a>` : null}
+    ${product.canBuy ? html` 
+    <a href="" id="buy-btn">Buy</a>` : null}
+ 
+  </div>
+</div>
+</section>`;
+ 
 export async function detailsPage(ctx) {
-    const productId = ctx.params.id
-    const product = await getProductById(productId);
-    const userId = getUserData()?._id
-    const isOwner = product._ownerId === userId;
-    ctx.render(detailsTemplate(product, isOwner, onDelete))
+    const id = ctx.params.id;
+    const product = await getProductById(id);
+    const userData = await getUserData();
+ 
+    if (userData && userData._id == product._ownerId) {
+        product.canEdit = true;
+    }
+ 
+    if (userData && userData._id != product._ownerId) {
+        product.canBuy = true;
+    }
+ 
+    ctx.render(detailsTemplate(product, onDelete));
+ 
     async function onDelete() {
-        const confirmed = confirm(`Are you sure you want to delete ${product.title}?`)
-        if (confirmed) {
-            await deleteProduct(productId);
+        const choice = confirm('Are you sure?');
+ 
+        if (choice) {
+            await deleteProduct(id);
             ctx.page.redirect('/my-products');
         }
     }
