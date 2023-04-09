25dc755d4a7381644b9df3d72979176047341e85
Martin Dogandzhiyski <94994594+MartinDogandzhiyski@users.noreply.github.com>
2023-04-09 20:45:15 +0300

    up

--- exam_prep_js_apps/src/data/offers.js
+++ exam_prep_js_apps/src/data/offers.js
@@ -1,29 +1,5 @@
 import { del, get, post, put } from "./api.js";
 
-// const endpoints = {
-//     catalog: '/data/offers?sortBy=_createdOn%20desc',
-//     byID: '/data/offers/',
-// };
-
-// export async function getAllOffers() {
-//     return get(endpoints.catalog);
-// }
-
-// export async function getById(id) {
-//     return get(endpoints.byID + id);
-// }
-
-// export async function createOffer(data) {
-//     return post(endpoints.catalog, data);
-// }
-
-// export async function editOffer(id, data) {
-//     return put(endpoints.byID + id, data);
-// }
-
-// export async function deleteOffer(id) {
-//     return del(endpoints.byID + id);
-// }
 
 const endPoints = {
     catalog: '/data/offers?sortBy=_createdOn%20desc',
